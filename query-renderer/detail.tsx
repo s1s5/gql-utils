8e3a5be4b12262119020feb6773fdb6213428cfb
Shogo Sawai <shogo.sawai@gmail.com>
2020-05-25 11:44:45 +0900

    refactor

--- query-renderer/detail.tsx
+++ query-renderer/detail.tsx
@@ -11,6 +11,7 @@ import {
     ReactRelayContext,
     QueryRenderer,
     FetchPolicy,
+    createFragmentContainer,
 } from 'react-relay'
 
 
@@ -26,14 +27,24 @@ export type RendererProps = {
     onLoading?: React.ReactNode
 }
 
+export function createFragmentRenderer<Props extends Object>(
+    // TODO: このkeyをなくせないか・・・
+    key: keyof Props, fragmentSpec: Record<string, GraphQLTaggedNode>) {
+    type P = Props & {
+        retry?: (() => void) | null
+        children: (data: Props[typeof key] | null, retry?: (() => void) | null) => any
+    }
+    const D = (props: P) => props.children(props[key] as (Props[typeof key] | null), props.retry)
+    // const D = (props: P) => props.children(props[key], props.retry)
+    return createFragmentContainer(D, fragmentSpec)
+}
+
 export function createDetailFC<TOperation extends OperationType, Fragment extends Object>(
-    query: GraphQLTaggedNode, cFC:any, key: keyof TOperation["response"], default_renderer_props?: RendererProps) {
-    const D = (props: any) => props.children(props[key])  // TODO: 型チェック
-    const F = cFC(D)
+    query: GraphQLTaggedNode, FR: any, options?: {query_key?: keyof TOperation["response"], fragment_key?: string, default_renderer_props?: RendererProps}) {
     return <T extends any>(props: QRProps<TOperation> & {children: (data: Fragment) => T} & RendererProps) => {
         const {environment, children, onError, onLoading, ...other_props} = props
-        let onError_ = onError ? onError : (default_renderer_props ? default_renderer_props.onError : undefined)
-        let onLoading_ = onLoading ? onLoading : (default_renderer_props ? default_renderer_props.onLoading : undefined)
+        let onError_ = onError ? onError : (options && options.default_renderer_props ? options.default_renderer_props.onError : undefined)
+        let onLoading_ = onLoading ? onLoading : (options && options.default_renderer_props ? options.default_renderer_props.onLoading : undefined)
 
         const context = React.useContext(ReactRelayContext)
         let e = environment ? environment : context!.environment
@@ -49,15 +60,20 @@ export function createDetailFC<TOperation extends OperationType, Fragment extend
             <QueryRenderer<TOperation>
               environment={e}
               query={q}
-              render={({error, props}) => {
-                      if (error || (props && (props[key] == null))) {
-                          const e = Error("アクセス権限がないか、予期しないエラーです")
+              render={({error, props, retry}) => {
+                      if (error) {
                           if (onError_ == null) {
-                              throw e
+                              throw error
                           }
-                          return onError_(error == null ? e : error)
+                          return onError_(error)
                       } else if (props) {
-                          return <F {...props}>{ children }</F>
+                          if (options && options.query_key && options.fragment_key) {
+                              const props_ = {
+                                  [options.fragment_key]: props[options.query_key]
+                              }
+                              return <FR retry={retry} {...props_}>{ children }</FR>
+                          }
+                          return <FR retry={retry} {...props}>{ children }</FR>
                       }
                       return onLoading_ ? onLoading_ : "読み込み中"
               } }
