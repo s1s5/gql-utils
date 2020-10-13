f64744ccdb19ea934d26dff43c0f916d02fc848d
Shogo Sawai <shogo.sawai@gmail.com>
2020-10-13 16:34:11 +0900

    remove form

--- form/form-field-props.tsx
+++
@@ -1,9 +0,0 @@
-type FormFieldProps<T> = {
-    formId: string,
-    value: T,
-    errors?: readonly string [],
-    onChange: (value: T) => void,
-    onUpload: (event: any) => void,
-}
-
-export {FormFieldProps}
--- index.tsx
+++ index.tsx
@@ -1,4 +1,3 @@
 export * from './environment'
-export * from './form'
 export * from './query-renderer'
 
